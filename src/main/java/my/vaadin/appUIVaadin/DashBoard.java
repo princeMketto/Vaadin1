1e5ea73bde166dfb2075e36048c6ec1b83ac101e
princeMketto <jmketto@gmail.com>
2018-02-07 11:30:15 +0300

    with modification

--- src/main/java/my/vaadin/appUIVaadin/DashBoard.java
+++ src/main/java/my/vaadin/appUIVaadin/DashBoard.java
@@ -21,6 +21,7 @@ import com.vaadin.ui.MenuBar.Command;
 import com.vaadin.ui.MenuBar.MenuItem;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.ProgressBar;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.themes.ValoTheme;
@@ -40,67 +41,67 @@ public class DashBoard extends VerticalLayout implements View {
 	MenuBar.Command command;
 	
 	public DashBoard(){
-	//	addStyleName(ValoTheme.PANEL_BORDERLESS);
 		setSizeFull();
-		setHeight("100%");
-		//setSpacing(false);
 		root = new GridLayout(1,4);
 		root.addComponents(header(),widgets(),navButtons(),lowPanels());
 		root.setSizeFull();
 		root.setSpacing(true);
 		setSizeFull();
-		setSpacing(true);
-	//	root.addStyleName("dashboard-view");
+		setMargin(new MarginInfo(false,false,false,false));
+		
 		setDefaultComponentAlignment(Alignment.TOP_CENTER);
 		setResponsive(true);
 		
 		Responsive.makeResponsive(this);
 		addComponent(root);
-		//addComponent(header());
-		/*addComponent(widgets());
-		addComponent(navButtons());
-		addComponent(lowPanels());*/
+		
 	}
 private Component lowPanels() {
 		GridLayout lowerSection = new GridLayout(3,1);
 		lowerSection.setSpacing(true);
-		VerticalLayout pan1 = new VerticalLayout();
-		pan1.setMargin(true);
-		pan1.setSpacing(false);
+		VerticalLayout leftpanel = new VerticalLayout();
+		leftpanel.addStyleName(ValoTheme.LAYOUT_CARD);
+		leftpanel.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		leftpanel.setMargin(true);
+		leftpanel.setSpacing(false);
 		
-		Button med = new Button("Medic service");
+		Button med = new Button("Medic service",VaadinIcons.DOCTOR_BRIEFCASE);
 		med.addStyleName(ValoTheme.BUTTON_BORDERLESS);
-		Button doc = new Button("doctors");
+		Button doc = new Button("doctors",VaadinIcons.DOCTOR);
 		doc.addStyleName(ValoTheme.BUTTON_BORDERLESS);
-		Button inter = new Button("intervation");
+		Button inter = new Button("intervation",VaadinIcons.STETHOSCOPE);
 		inter.addStyleName(ValoTheme.BUTTON_BORDERLESS);
-		pan1.addComponents(med,doc,inter);
-		pan1.addStyleName("pan1");
+		leftpanel.addComponents(med,doc,inter);
+		leftpanel.addStyleName("pan1");
 		
-		Button purch = new Button("Purchases");
+		Button purch = new Button("Purchases",VaadinIcons.CREDIT_CARD);
 		purch.addStyleName(ValoTheme.BUTTON_BORDERLESS);
 		purch.addStyleName("btnDash");
-		Button inv = new Button("Inventory");
+		Button inv = new Button("Inventory",VaadinIcons.ARCHIVES);
 		inv.addStyleName(ValoTheme.BUTTON_BORDERLESS);
 		inv.addStyleName("btnDash");
-		Button supp = new Button("Supplier");
+		Button supp = new Button("Supplier",VaadinIcons.CART_O);
 		supp.addStyleName(ValoTheme.BUTTON_BORDERLESS);
-		VerticalLayout pan2 = new VerticalLayout(purch,inv,supp);
-		pan2.setMargin(true);
-		pan2.setSpacing(false);
+		VerticalLayout middlepanel = new VerticalLayout(purch,inv,supp);
+		middlepanel.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		middlepanel.addStyleName(ValoTheme.LAYOUT_CARD);
+		middlepanel.setMargin(true);
+		middlepanel.setSpacing(false);
 		
 		
-		Button nam = new Button("User name");
+		Button nam = new Button("User name",VaadinIcons.USER);
 		nam.addStyleName(ValoTheme.BUTTON_BORDERLESS);
-		Button mail = new Button("user mail");
+		Button mail = new Button("user mail",VaadinIcons.MAILBOX);
 		mail.addStyleName(ValoTheme.BUTTON_BORDERLESS);
-		Button phon = new Button("user phone");
+		Button phon = new Button("user phone",VaadinIcons.PHONE);
 		phon.addStyleName(ValoTheme.BUTTON_BORDERLESS);
-		VerticalLayout pan3 = new VerticalLayout(nam,mail,phon);
-		pan3.setMargin(true);
-		pan3.setSpacing(false);
+		VerticalLayout rightpanel = new VerticalLayout(nam,mail,phon);
+		rightpanel.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		rightpanel.addStyleName(ValoTheme.LAYOUT_CARD);
+		rightpanel.setMargin(true);
+		rightpanel.setSpacing(false);
 		
-		lowerSection.addComponents(pan1,pan2,pan3);
+		lowerSection.addComponents(leftpanel,middlepanel,rightpanel);
 		lowerSection.setWidth("100%");
 		
 		
@@ -143,91 +144,141 @@ private Component navButtons() {
 		return flat;
 	}
 private Component widgets() {
-	GridLayout widget = new GridLayout(3,1);
-	VerticalLayout ana = new VerticalLayout();
-	Label ons = new Label("Sales within 7 Days");
-	Label onp = new Label("Purchase within 7Days");
-	Label com = new Label("comment");
-	ana.addComponents(ons,onp,com);
-	ana.setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
-	ana.setMargin(true);
+	HorizontalLayout widget = new HorizontalLayout();
+	VerticalLayout analyticPanel = new VerticalLayout();
+	analyticPanel.setHeight("200px");
+	analyticPanel.setWidth("400px");
+	ProgressBar saleAnalyser = new ProgressBar();
+	saleAnalyser.setWidth("80%");
+	saleAnalyser.setCaption("Sales within 7 Days");
 	
-	VerticalLayout widgs = new VerticalLayout();
-		HorizontalLayout flatwgs = new HorizontalLayout();
+	ProgressBar purchaseAnalyser = new ProgressBar();
+	purchaseAnalyser.setWidth("80%");
+	purchaseAnalyser.setCaption("Purchase within 7 Days");
+	Label comment = new Label("You have good Sales past week");
+	
+	comment.addStyleNames(ValoTheme.LABEL_BOLD, ValoTheme.LABEL_COLORED, ValoTheme.LABEL_H4);
+	
+	analyticPanel.addComponents(saleAnalyser,purchaseAnalyser,comment);
+	analyticPanel.setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
+	analyticPanel.setMargin(false);
+	analyticPanel.addStyleName(ValoTheme.LAYOUT_CARD);
+	
+	VerticalLayout centerwidgets = new VerticalLayout();
+	centerwidgets.setWidth("500px");
+	centerwidgets.setHeight("200px");
+		HorizontalLayout sale_expensewidgets = new HorizontalLayout();
 		
-		Label tS =new Label("Sale");
-		tS.addStyleName(ValoTheme.LABEL_H2);
-		HorizontalLayout card0 = new HorizontalLayout();
-		card0.addStyleName(MaterialTheme.CARD_0);
-		//card0.setSpacing(true);
-		Label ic = new Label("");
-		ic.setStyleName(MaterialTheme.LABEL_H1+""+MaterialTheme.LABEL_COLORED);
-		ic.setIcon(VaadinIcons.MONEY);
 		
-		card0.addComponent(ic);
-		card0.addComponent(tS);
+		HorizontalLayout salewidget = new HorizontalLayout();
+		salewidget.addStyleName(ValoTheme.LAYOUT_CARD);
+		salewidget.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		salewidget.setWidth("150px");
+		salewidget.setHeight("75px");
+		Label salelabel =new Label("0.0"); //today sale
+		salelabel.addStyleName(ValoTheme.LABEL_H2);
+		Label icon = new Label("");
+		icon.setStyleName("blueicon");
+		icon.setIcon(VaadinIcons.MONEY);
 		
-		Label tp =new Label("Sale");
-		tp.addStyleName(ValoTheme.LABEL_H2);
-		HorizontalLayout card1 = new HorizontalLayout();
-		card1.addStyleName(MaterialTheme.CARD_0);
-		//card0.setSpacing(true);
-		Label ic1 = new Label("");
-		ic1.setStyleName(MaterialTheme.LABEL_H1+""+MaterialTheme.LABEL_COLORED);
-		ic1.setIcon(VaadinIcons.MINUS_CIRCLE);
+		salewidget.addComponent(icon);	
+		salewidget.addComponent(salelabel);
 		
-		card1.addComponent(ic1);
-		card1.addComponent(tp);
+		//second Widget
+		HorizontalLayout expensewidget = new HorizontalLayout();
+		expensewidget.addStyleName(ValoTheme.LAYOUT_CARD);
+		expensewidget.setWidth("150px");
+		expensewidget.setHeight("75px");
+		Label expenselabel =new Label("1000");
+		expenselabel.addStyleName(ValoTheme.LABEL_H2);
+		Label iconExpense = new Label("");
+		iconExpense.setStyleName(ValoTheme.LABEL_COLORED);
+		iconExpense.setIcon(VaadinIcons.MINUS_CIRCLE_O);
 		
-		flatwgs.addComponents(card0,card1);
-		flatwgs.setWidth("100%");
+		expensewidget.addComponent(iconExpense);
+		expensewidget.addComponent(expenselabel);
 		
-		widgs.addComponent(flatwgs);
-		Panel cloc = new Panel("",new Label("Display clock \n and or ...\n Here"));
-		cloc.setHeight("20%");
+		//third Widget
+				HorizontalLayout modewidget = new HorizontalLayout();
+				modewidget.addStyleName(ValoTheme.LAYOUT_CARD);
+				modewidget.setWidth("160px");
+				modewidget.setHeight("75px");
+				Label modelabel =new Label("Online");
+				modelabel.addStyleName(ValoTheme.LABEL_H2);
+				//card0.setSpacing(true);
+				Label iconMode = new Label("");
+				iconMode.setStyleName(ValoTheme.LABEL_COLORED);
+				iconMode.setIcon(VaadinIcons.SIGNAL);
+				
+				modewidget.addComponent(iconMode);
+				modewidget.addComponent(modelabel);
+		
+		sale_expensewidgets.addComponents(salewidget,expensewidget,modewidget);
+		
+		
+		HorizontalLayout cloc = new HorizontalLayout();
+		Label displayclock = new Label("Wednesday 2/7/2018, 11:00am");
+		displayclock.addStyleNames(ValoTheme.LABEL_BOLD,ValoTheme.LABEL_COLORED,ValoTheme.LABEL_H3);
+		displayclock.setIcon(VaadinIcons.CLOCK);
+		
+		cloc.addComponent(displayclock);
+		cloc.setComponentAlignment(displayclock, Alignment.MIDDLE_CENTER);
+		cloc.addStyleName(ValoTheme.LAYOUT_CARD);
+		cloc.setHeight("90px");
 		cloc.setWidth("100%");
-		widgs.addComponent(cloc);
-		widgs.setWidth("100%");
 		
-		 //LEFT
-		VerticalLayout stat = new VerticalLayout();
-		Label it = new Label("SUM Item in store");
-		Label itex = new Label("Item expired");
-		Label totit = new Label("Total items");
-		stat.addComponents(it,itex,totit);
-		stat.setMargin(true);
-		stat.setWidth("100%");
-		stat.setDefaultComponentAlignment(Alignment.MIDDLE_RIGHT);
-		stat.setMargin(new MarginInfo(true,false,false,false));
-		//Add in Layout
-		widget.addComponents(ana,widgs,stat);
+		centerwidgets.addComponents(sale_expensewidgets,cloc);
+		centerwidgets.setMargin(new MarginInfo(false,false,false,false));
+		centerwidgets.setSpacing(true);
+		
+		
+		
+		 //LEFT PANEL
+		VerticalLayout status = new VerticalLayout();
+		status.addStyleName(ValoTheme.LAYOUT_CARD);
+		Label itemstore = new Label("SUM Item in store");
+		Label itemexpired = new Label("Item expired");
+		Label totalitem = new Label("Total items");
+		status.addComponents(itemstore,itemexpired,totalitem);
+		status.setWidth("300px");
+		status.setHeight("200px");
+		status.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		status.setMargin(new MarginInfo(false,false,false,false));
+		
+		//Add in ROOT Layout
+		
+		widget.addComponents(analyticPanel,centerwidgets,status);
 		widget.setWidth("100%");
-		widget.setComponentAlignment(stat, Alignment.MIDDLE_RIGHT);
-		widget.setComponentAlignment(ana, Alignment.MIDDLE_LEFT);
+		widget.setComponentAlignment(status, Alignment.MIDDLE_RIGHT);
+		widget.setComponentAlignment(analyticPanel, Alignment.MIDDLE_LEFT);
 		
 		return widget;
-	}
+	}	
 private Component header() {
-	GridLayout header = new GridLayout(2,1);
-	
-	HorizontalLayout toolL = new HorizontalLayout();
-	toolL.setDefaultComponentAlignment(Alignment.TOP_LEFT);
-	toolL.setWidth("100%");
-	HorizontalLayout toolR = new HorizontalLayout();
-	toolR.setDefaultComponentAlignment(Alignment.TOP_RIGHT);
-	toolR.setWidth("100%");
-	toolR.setSpacing(false);
-	
+	HorizontalLayout uppersection = new HorizontalLayout();
+	uppersection.setHeight("40px");
+	uppersection.setWidth("100%");
+	uppersection.setMargin(new MarginInfo(false,false,false,false));
+	uppersection.addStyleName(ValoTheme.LAYOUT_CARD);
+	HorizontalLayout toolLeft = new HorizontalLayout();
+	uppersection.addComponent(toolLeft);
+	toolLeft.setDefaultComponentAlignment(Alignment.TOP_LEFT);
+	toolLeft.setWidth("100%");
+	HorizontalLayout toolRight = new HorizontalLayout();
+	toolRight.setDefaultComponentAlignment(Alignment.TOP_RIGHT);
+	toolRight.setWidth("100%");
+	toolRight.setSpacing(false);
+	uppersection.addComponent(toolRight);
 	
 	title = new Label("Dashboard");
 	
 	title.addStyleName(ValoTheme.LABEL_H2);
 	title.addStyleName(ValoTheme.LABEL_NO_MARGIN);
-	toolL.addComponent(title);
+	toolLeft.addComponent(title);
 	
 	MenuBar bar = new MenuBar();
 	bar.addStyleName("mybarmenu");
-	toolR.addComponent(bar);
+	toolRight.addComponent(bar);
 	
 	MenuItem m1 = bar.addItem("Feedback", VaadinIcons.COMMENTS_O, command);
 	MenuItem m2 = bar.addItem("Help", VaadinIcons.QUESTION_CIRCLE_O, command);
@@ -239,15 +290,11 @@ private Component header() {
 		    }
 	};
 	
-	//toolR.addComponents(b1,b2,b3);
-	
-	header.addComponents(toolL,toolR);
-	header.setComponentAlignment(toolR, Alignment.MIDDLE_RIGHT);
-	header.setWidth("100%");
-	header.setHeight("15%");
+	uppersection.setComponentAlignment(toolRight, Alignment.MIDDLE_RIGHT);
+	uppersection.setWidth("100%");
 	
 	
-		return header;
+		return uppersection;
 	}
 
 @Override
